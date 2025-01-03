ef87b61a10abc507d638132f26447e2713f3ad8b
Velizar Ganchev <velizar_ganchev@hotmail.com>
2025-01-03 08:44:19 +0100

    Update signals to include pre_save for video file management and enhance auto-delete functionality

--- content_app/signals.py
+++ content_app/signals.py
@@ -1,5 +1,5 @@
 import os
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from .models import Video
 
@@ -20,3 +20,23 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     if instance.video_file:
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
+
+@receiver(pre_save, sender=Video)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Video.objects.get(pk=instance.pk).video_file
+    except Video.DoesNotExist:
+        return False
+
+    new_file = instance.video_file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
